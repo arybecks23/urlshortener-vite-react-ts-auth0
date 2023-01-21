9eaaca8d7d684fad0de19a20f81b7d4f2efc41a0
Arman <arybecks23@gmail.com>
2023-01-21 01:01:27 -0800

    Revert "Fix the automatic login after a logout"
    This reverts commit 2209b7d740ac1d2d9ea32add89f37bb0ac7b0d7e.

--- src/components/layout/DashboardNav.tsx
+++ src/components/layout/DashboardNav.tsx
@@ -6,13 +6,11 @@ type Props = {};
 
 const DashboardNav = (props: Props) => {
   const { user, logout } = useAuth0();
-
   if (user) {
     return (
       <Disclosure as="nav" className="bg-cyan-600">
         {({ open }) => (
           <>
-            <a href="https://armanist.us.auth0.com/v2/logout">Logout</a>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
@@ -50,7 +48,7 @@ const DashboardNav = (props: Props) => {
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
                             <a
-                              href="https://armanist.us.auth0.com/v2/logout"
+                              href="/#"
                               onClick={() =>
                                 logout({
                                   returnTo: window.location.origin,

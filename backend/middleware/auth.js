ea943670d4c0c8791f25662e69757c3f39588166
Arman <arybecks23@gmail.com>
2023-01-22 00:35:57 -0800

    Get the values from env variables

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,9 @@
 const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 const checkJwt = auth({
-  audience: "http://localhost:5600",
-  issuerBaseURL: `https://armanist.us.auth0.com/`,
-  // audience: process.env.AUTH0_API_IDENTIFIER,
-  // issuerBaseURL: process.env.AUTH0_ISSUER,
+  // audience: "http://localhost:5600",
+  // issuerBaseURL: `https://armanist.us.auth0.com/`,
+  audience: process.env.AUTH0_API_IDENTIFIER,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
 });
 
 module.exports = { checkJwt };

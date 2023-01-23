36ef2cf02f68c9847f638b40df77e542ec7455dc
Arman <arybecks23@gmail.com>
2023-01-22 22:15:44 -0800

    fix the login redirect

--- src/components/layout/DashboardNav.tsx
+++ src/components/layout/DashboardNav.tsx
@@ -48,7 +48,7 @@ const DashboardNav = (props: Props) => {
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
                             <a
-                              href="https://armanist.us.auth0.com/v2/logout"
+                              href={import.meta.env.VITE_AUTH0_LOGOUT_URL}
                               onClick={() =>
                                 logout({
                                   returnTo: window.location.origin,
